ca146da4d354fe757fc5800e686fd28009ca0cd4
Ivlina Moroshkina <ivlina@Ivlinas-MacBook-Air.local>
2023-01-18 00:03:00 +0300

    Working on task 8.

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1
+let cleanerSalary = 40000;
+let teacher = 22000;
+console.log(`Вечерний уборщик, имея голос и педагогическое образование, может зарабатывать ${cleanerSalary + teacher} рублей.`);
+
+// 2
+console.log('Владивосток' + ' ' + 2000);
+
+// 3
+console.log(`Русалка прокрастинировала ${128 + 254 + 137 + 201} минут.`);
+
+// 4
+console.log(2 ** 5);
+
+// 5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+// 6
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ", " + name);
+
+// 7
+let time;
+time = 34;
+time -= 15; 
+time -=10;
+time = 0
+console.log('Вы приехали. Добро пожаловать в Москву!');
+
+// 8
+let fahrenheitи = 451;
+let celsius = (fahrenheit - 32) / 1.8;;
